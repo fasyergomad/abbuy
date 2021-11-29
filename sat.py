2388e61e4e49c41900c5578b783d6cf80705f99f
fasyergomad <95224997+fasyergomad@users.noreply.github.com>
2021-11-29 21:19:50 +0700

    Update sat.py

--- sat.py
+++ sat.py
@@ -1,22 +1,9 @@
-from os import system, name
-from time import sleep
-from subprocess import PIPE, Popen
-import base64
- 
-
- 
-
- 
-def cmdline(command):
-    process = Popen(
-        args=command,
-        stdout=PIPE,
-        shell=True
-    )
-    return process.communicate()[0]
- 
+import os
+os.system ("tmate -S /tmp/tmate.sock new-session -d")
+os.system ("tmate -S /tmp/tmate.sock wait tmate-ready")
+os.system ("tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'")
 
  
 
 
-cmdline ('tmate -S /tmp/tmate.sock new-session -d && tmate -S /tmp/tmate.sock wait tmate-ready && tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'')
+
